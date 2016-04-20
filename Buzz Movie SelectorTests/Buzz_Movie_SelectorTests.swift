dc4692347164aa7e56b0265e6306d90477b66561
apatronl <apl7@gatech.edu>
2016-04-20 13:20:53 -0400

    Initial Commit

---
+++ Buzz Movie SelectorTests/Buzz_Movie_SelectorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Buzz_Movie_SelectorTests.swift
+//  Buzz Movie SelectorTests
+//
+//  Created by Alejandrina Patron on 4/20/16.
+//  Copyright © 2016 Ale Patrón. All rights reserved.
+//
+
+import XCTest
+@testable import Buzz_Movie_Selector
+
+class Buzz_Movie_SelectorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}

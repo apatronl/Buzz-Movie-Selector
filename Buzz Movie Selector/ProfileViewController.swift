55a4a4e288369b6e683a1cb7b10b632642a2773f
apatronl <apl7@gatech.edu>
2016-04-26 13:14:40 -0400

    Added README

--- Buzz Movie Selector/ProfileViewController.swift
+++ Buzz Movie Selector/ProfileViewController.swift
@@ -11,10 +11,15 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var username: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let user = User(name: "Ale Patron", username: "ale_patron", password: "tech", profilePicture: nil)
+        let user = User(name: "Ale Patron", username: "ale", password: "tech", profilePicture: nil)
+        name.text = user.name
+        username.text = "@" + user.username
     }
     
     override func didReceiveMemoryWarning() {
---
+++ README.md
@@ -0,0 +1,11 @@
+# Buzz Movie Selector
+Partial iOS implementation of Buzz Movie Selector, a movie search and rating app for Georgia Tech’s CS 2340 class (Objects and Design).
+
+#HTTP Requests
+HTTP requests are handled using the [Alamofire](https://github.com/Alamofire/Alamofire) library.
+
+#JSON Parsing
+To parse the information obtained from the requests, I used the [SwiftyJSON](https://github.com/SwiftyJSON/SwiftyJSON) library.
+
+#Movie Information
+All the results shown in this app are obtained using the [Rotten Tomatoes](https://developer.rottentomatoes.com/) movie search API.
